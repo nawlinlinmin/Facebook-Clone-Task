68d78806b7a92b22f20eebd73ebfc673bfc21937
naw linlin min <nodmentor1ygn@gmail.com>
2020-10-05 16:58:48 +0630

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_180931) do
+ActiveRecord::Schema.define(version: 2020_10_05_101356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "feeds", force: :cascade do |t|
     t.text "image"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
